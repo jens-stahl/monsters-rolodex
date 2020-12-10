534fa4b03b56fa130e2be9b4c7c3f9a461a6f82a
Jens Stahl <mail@jens-stahl.de>
2020-12-10 16:45:32 +0100

    format

--- src/App.js
+++ src/App.js
@@ -12,20 +12,23 @@ class App extends Component {
             searchField: ''
         }
     }
+
     componentDidMount() {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
             .then(users => this.setState({monsters: users}));
     }
+
     render() {
-        const {monsters,searchField} = this.state;
+        const {monsters, searchField} = this.state;
         const filteredMonsters = monsters.filter(monster =>
             monster.name.toLowerCase().includes(searchField.toLowerCase())
         );
         return (
             <div className="App">
-            <SearchBox placeholder='Jetzt suchen!' handleChange={event => this.setState({searchField:event.target.value})}/>
-                <CardList monsters={filteredMonsters }/>
+                <SearchBox placeholder='Jetzt suchen!'
+                           handleChange={event => this.setState({searchField: event.target.value})}/>
+                <CardList monsters={filteredMonsters}/>
             </div>
         )
     }
